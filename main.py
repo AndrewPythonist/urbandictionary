169f157878f30e67428a3da0bb4f64b07e5696e1
wixl <builddig22@gmail.com>
2020-02-09 19:54:39 +0200

    feat: created main class

--- main.py
+++ main.py
@@ -1,34 +1,4 @@
-from src.get_content import get_random_word, get_content
-from src.wall_posting import posting
-from src.draw_card import save_card
-from time import sleep
-from random import randint
-
-# Введите здесь название вашего конфиг-файла, где будут храниться token, group_id, album_id.
-CONFIG = 'vk_info'
-
-exec(f'from {CONFIG} import token, group_id, album_id')
-
-def main(timespan):
-    '''Основная функция программы: Карточки посятся на стену каждые timespan секунд'''
-    while True:
-        word = get_random_word()
-        content = get_content(word)
-
-        path = save_card(
-            word=word,
-            image=f'data/template/backgroundimages/bgimg ({randint(1,9)}).jpg'
-            )
-
-        posting(
-            token=token,
-            imagepath=path,
-            group_id=group_id,
-            album_id=album_id,
-            message=f"#{content['category']}@urbandictionary" if content['category'] else ''
-            )
-
-        sleep(timespan)
+from src.urbandictionary import UrbanDictionary
 
 if __name__ == '__main__':
-    main(17280)
+    program = UrbanDictionary()
---
+++ src/urbandictionary.py
@@ -0,0 +1,3 @@
+class UrbanDictionary:
+    def __init__(self):
+        pass
