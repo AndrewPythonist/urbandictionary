ce22c1f004f93e7169ba1f9dcc81ae575f4b3615
wixl <builddig22@gmail.com>
2020-02-09 20:04:12 +0200

    feat: added singleton init to main class

--- src/urbandictionary.py
+++ src/urbandictionary.py
@@ -1,3 +1,13 @@
+from src.common.logger import get_logger
+from src.singleton.the import The
+
+logger = get_logger(__name__)
+
+
 class UrbanDictionary:
     def __init__(self):
+        logger.warning("starting UrbanDictionary")
+        self.the = The()
+        self.the.start_services()
+
         pass
