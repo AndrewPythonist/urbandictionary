8f9eb960396ea8aaf635f5cc92255874d17bf450
wixl <builddig22@gmail.com>
2020-02-09 22:53:09 +0200

    fix: added import locally

--- src/singleton/the.py
+++ src/singleton/the.py
@@ -1,6 +1,3 @@
-from src.services.cardPlacerService import CardPlacerService
-from src.services.configService import ConfigService
-from src.services.dataSetService import DataSetService
 from src.singleton.singleton import Singleton
 
 """"
@@ -17,11 +14,16 @@ class The(Singleton):
         """
         if not self.__was_initialized:
             self.__was_initialized = True
+
+            from src.services.cardPlacerService import CardPlacerService
+            from src.services.configService import ConfigService
+            from src.services.dataSetService import DataSetService
+
             self.config_service = ConfigService()
-            self.card_placer_service = CardPlacerService()
             self.data_set_service = DataSetService()
+            self.card_placer_service = CardPlacerService()
 
     def start_services(self):
         self.config_service.start()
-        self.card_placer_service.start()
         self.data_set_service.start()
+        # self.card_placer_service.start()
