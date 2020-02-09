8161708f308bd77982caa03bc2309ab92c8c0a40
wixl <builddig22@gmail.com>
2020-02-09 20:35:14 +0200

    feat: added loop for card_placed_service

--- src/services/cardPlacerService.py
+++ src/services/cardPlacerService.py
@@ -1,9 +1,45 @@
+import threading
+import time
+
+from src.draw_card import save_card
+from src.get_content import get_random_word, get_content
 from src.services.baseService import BaseService
+from src.singleton.the import The
+from src.wall_posting import posting
+import random
 
 
 class CardPlacerService(BaseService):
     def start(self):
-        pass
+        threading.Thread(name="card_placer_loop", target=self.place_loop).start()
 
     def __init__(self):
         pass
+
+    @staticmethod
+    def place_loop():
+        the = The()
+
+        while True:
+            word = get_random_word()
+            content = get_content(word)
+
+            path = save_card(
+                word=word,
+                image=f'data/template/backgroundimages/bgimg ({random.randint(1, 9)}).jpg'
+            )
+
+            token = the.config_service.data["token"].value
+            group_id = the.config_service.data["group_id"].value
+            album_id = the.config_service.data["album_id"].value
+            sleep_for = the.config_service.data["time_span"].value
+
+            posting(
+                token=token,
+                imagepath=path,
+                group_id=group_id,
+                album_id=album_id,
+                message=f"#{content['category']}@urbandictionary" if content['category'] else ''
+            )
+
+            time.sleep(sleep_for)
--- src/singleton/the.py
+++ src/singleton/the.py
@@ -1,3 +1,4 @@
+from src.services.cardPlacerService import CardPlacerService
 from src.services.configService import ConfigService
 from src.singleton.singleton import Singleton
 
@@ -16,6 +17,8 @@ class The(Singleton):
         if not self.__was_initialized:
             self.__was_initialized = True
             self.config_service = ConfigService()
+            self.card_placer_service = CardPlacerService()
 
     def start_services(self):
         self.config_service.start()
+        self.card_placer_service.start()
