0cdaaf9a956bd6246c7c30a9385d1fe398080bef
wixl <builddig22@gmail.com>
2020-02-09 22:42:13 +0200

    feat: added dataSetService to singleton

--- src/singleton/the.py
+++ src/singleton/the.py
@@ -1,5 +1,6 @@
 from src.services.cardPlacerService import CardPlacerService
 from src.services.configService import ConfigService
+from src.services.dataSetService import DataSetService
 from src.singleton.singleton import Singleton
 
 """"
@@ -18,7 +19,9 @@ class The(Singleton):
             self.__was_initialized = True
             self.config_service = ConfigService()
             self.card_placer_service = CardPlacerService()
+            self.data_set_service = DataSetService()
 
     def start_services(self):
         self.config_service.start()
         self.card_placer_service.start()
+        self.data_set_service.start()
